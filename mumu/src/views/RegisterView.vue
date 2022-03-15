68cd86e32271f9e03e9c2e870cc2de259ea06acd
idwooin <81429237+idwooin@users.noreply.github.com>
2022-03-15 20:05:01 +0900

    first commit
    firsttttt

---
+++ mumu/src/views/RegisterView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="registerpanel">
+    <h1> Sign Up </h1>
+    <div class="txt">
+      <h3>First Name</h3>
+      <input id="firstname" type="text" placeholder="First Name" />
+    </div>
+    <div class="txt">
+      <h3>Last Name</h3>
+      <input id="lastname" type="text" placeholder="Last Name" />
+    </div>
+    <div class="txt">
+      <h3>Email</h3>
+      <input id="email" type="text" placeholder="Email" />
+    </div>
+    <div class="txt">
+      <h3>Last Name</h3>
+      <input id="password" type="text" placeholder="Password" />
+    </div>
+    <div class="txt">
+      <h3>Last Name</h3>
+      <input id="confirmpassword" type="text" placeholder="Confirm Password" />
+    </div>
+    <div class="buttons">
+      <input type="button" value="Register"/>
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+export default{
+  name: 'RegisterView',
+  components: {
+  },
+}
+</script>
